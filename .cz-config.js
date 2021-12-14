7bdf062cd971b70f4ebdf7d02cc3e232f87b024d
xzksj <930233132@qq.com>
2021-12-14 17:15:59 +0800

    feat(git): git跳过部分内容提交

--- .cz-config.js
+++ .cz-config.js
@@ -14,7 +14,7 @@ module.exports = {
 
   ],
   // 步骤
-  message: {
+  messages: {
     type: '请选择提交的类型',
     customScope: '请输入修改的范围（可选）',
     subject: '请简要描述提交（必填）',
@@ -22,6 +22,9 @@ module.exports = {
     footer: '请输入要关闭的issue（可选）',
     confirmCommit: '确认要使用以上信息提交？(y/n)'
   },
+
+  // 跳过问题
+  skipQuestions: ['body', 'footer'],
   // 默认长度为72
   subjectLimit: 72
 }
