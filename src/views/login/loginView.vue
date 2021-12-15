f717cd89c055bfd3cb55fd425093a8733d508c82
xzksj <930233132@qq.com>
2021-12-15 20:20:30 +0800

    feat(登录模块): 登录界面完成

---
+++ src/views/login/loginView.vue
@@ -0,0 +1,202 @@
+<template>
+  <div class="login-container">
+    <el-form class="login-form" :model="loginForm" :rules="loginRules">
+      <div class="title-container">
+        <h3 class="title">用户登陆</h3>
+      </div>
+      <!--      username-->
+      <el-form-item prop="username">
+        <span class="svg-container">
+          <svg-icon icon="user"></svg-icon>
+        </span>
+        <el-input
+          plascholder="username"
+          name="username"
+          v-model="loginForm.username"
+          type="test"
+        ></el-input>
+      </el-form-item>
+      <!--      password-->
+      <el-form-item prop="password">
+        <span class="svg-container">
+          <svg-icon icon="password"></svg-icon>
+        </span>
+        <el-input
+          plascholder="password"
+          name="password"
+          :type="passwordType"
+          v-model="loginForm.password"
+        ></el-input>
+        <span class="show-pwd">
+          <span class="svg-container" @click="onChangePwdType">
+            <svg-icon :icon="eyeIcon"></svg-icon>
+          </span>
+        </span>
+      </el-form-item>
+      <el-button style="width: 100%; margin-bottom: 30px">登录</el-button>
+    </el-form>
+  </div>
+</template>
+<script setup>
+import SvgIcon from '@/components/SvgIcon/index'
+import { validatePassword } from './rules'
+import { ref, computed } from 'vue'
+// 数据源
+const loginForm = ref({
+  username: 'super-admin',
+  password: '123456'
+})
+// 验证规则
+const loginRules = ref({
+  username: [
+    {
+      required: true,
+      trigger: 'blur',
+      message: '用户名为必填项'
+    }
+  ],
+  password: [
+    {
+      required: true,
+      trigger: 'blur',
+      validator: validatePassword()
+    }
+  ]
+})
+
+// 处理密码框文本显示
+const passwordType = ref('password')
+
+const eyeIcon = computed(() =>
+  passwordType.value === 'password' ? 'eye' : 'eye-open'
+)
+
+// tempalate 中绑定的方法，直接声明即可
+const onChangePwdType = () => {
+  if (passwordType.value === 'password') {
+    passwordType.value = 'text'
+  } else {
+    passwordType.value = 'password'
+  }
+}
+</script>
+
+<style lang="scss">
+$bg: #283443;
+$light_gray: #fff;
+$cursor: #fff;
+
+@supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
+  .login-container .el-input input {
+    color: $cursor;
+  }
+}
+
+/* reset element-ui css */
+.login-container {
+  .el-input {
+    display: inline-block;
+    height: 47px;
+    width: 85%;
+
+    input {
+      background: transparent;
+      border: 0px;
+      -webkit-appearance: none;
+      border-radius: 0px;
+      padding: 12px 5px 12px 15px;
+      color: $light_gray;
+      height: 47px;
+      caret-color: $cursor;
+
+      &:-webkit-autofill {
+        box-shadow: 0 0 0px 1000px $bg inset !important;
+        -webkit-text-fill-color: $cursor !important;
+      }
+    }
+  }
+
+  .el-form-item {
+    border: 1px solid rgba(255, 255, 255, 0.1);
+    background: rgba(0, 0, 0, 0.1);
+    border-radius: 5px;
+    color: #454545;
+  }
+}
+</style>
+
+<style lang="scss" scoped>
+$bg: #2d3a4b;
+$dark_gray: #889aa4;
+$light_gray: #eee;
+$cursor: #fff;
+
+.login-container {
+  min-height: 100%;
+  width: 100%;
+  background-color: $bg;
+  overflow: hidden;
+
+  .login-form {
+    position: relative;
+    width: 520px;
+    max-width: 100%;
+    padding: 160px 35px 0;
+    margin: 0 auto;
+    overflow: hidden;
+  }
+
+  .tips {
+    font-size: 14px;
+    color: #fff;
+    margin-bottom: 10px;
+
+    span {
+      &:first-of-type {
+        margin-right: 16px;
+      }
+    }
+  }
+
+  .svg-container {
+    padding: 6px 5px 6px 15px;
+    color: $dark_gray;
+    vertical-align: middle;
+    width: 30px;
+    display: inline-block;
+  }
+
+  .title-container {
+    position: relative;
+
+    .title {
+      font-size: 26px;
+      color: $light_gray;
+      margin: 0px auto 40px auto;
+      text-align: center;
+      font-weight: bold;
+    }
+  }
+
+  .show-pwd {
+    position: absolute;
+    right: 10px;
+    font-size: 16px;
+    color: $dark_gray;
+    cursor: pointer;
+    user-select: none;
+  }
+
+  .thirdparty-button {
+    position: absolute;
+    right: 0;
+    bottom: 6px;
+  }
+
+  @media only screen and (max-width: 470px) {
+    .thirdparty-button {
+      display: none;
+    }
+  }
+}
+</style>
